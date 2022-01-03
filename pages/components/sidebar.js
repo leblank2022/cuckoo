f59e8cc574d918a94d751143c2505b21b85f75d0
LeBlank <leblank2022@gmail.com>
2022-01-03 16:37:35 +0800

    add initial scrolls

--- pages/components/sidebar.js
+++ pages/components/sidebar.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { slide as Menu } from "react-burger-menu";
+import { Image, Icon } from "semantic-ui-react";
+import Link from "next/link";
 
-export class Sidebar extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default class Sidebar extends Component {
+  componentDidMount() {
+    console.log(this.props.active);
+  }
+  render() {
+    return (
+      <Menu {...this.props}>
+        <div className="bm-item nav-content">
+          <Image src="/assets/logo/cuckoo_club_logo.png" className="logo" />
+          <div className="links">
+            <Link href="/">
+              <a className="menu-item active">Home</a>
+            </Link>
+            <Link href="/">
+              <a className="menu-item">Roadmap</a>
+            </Link>
+            <Link href="/">
+              <a className="menu-item">Team</a>
+            </Link>
+          </div>
+          <div className="social-icons">
+            <a title="Medium" href="https://medium.com/" target="_blank">
+              <Icon name="medium"></Icon>
+            </a>
+            <a title="Youtube" href="https://www.youtube.com/" target="_blank">
+              <Icon name="youtube square"></Icon>
+            </a>
+          </div>
+        </div>
+      </Menu>
+    );
+  }
 }
-
-export default Sidebar
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,109 @@
 import React, { Component } from "react";
+import Sidebar from "./components/sidebar";
+import ReactFullpage from "@fullpage/react-fullpage";
+import {
+  Container,
+  Header,
+  Image,
+  Icon,
+  Button,
+  Card,
+  Grid,
+  Divider,
+  Label,
+} from "semantic-ui-react";
 
 export class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen });
+  }
+  closeMenu() {
+    this.setState({ menuOpen: false });
+  }
+  toggleMenu() {
+    this.setState((state) => ({
+      menuOpen: !state.menuOpen,
+    }));
+  }
   render() {
-    return <div>Cuckoo</div>;
+    return (
+      <div id="App">
+        <Sidebar
+          pageWrapId={"full-screen"}
+          outerContainerId={"App"}
+          isOpen={this.state.menuOpen}
+          onStateChange={(state) => this.handleStateChange(state)}
+          active={"projects"}
+        />
+        <main id="full-screen">
+          <div className="light-nav">
+            <Container>
+              <div className="header-content">
+                <div>
+                  <Header as="h2">
+                    <Image src="/assets/logo/cuckoo_club_logo.png" />
+                    <Header.Content>Cuckoo Club</Header.Content>
+                  </Header>
+                </div>
+                <div>
+                  <Button
+                    inverted
+                    icon
+                    size="big"
+                    onClick={() => this.toggleMenu()}
+                  >
+                    <Icon name="bars" />
+                  </Button>
+                </div>
+              </div>
+            </Container>
+          </div>
+          <div className="projects-page">
+            <ReactFullpage
+              navigation
+              render={(comp) =>
+                console.log("render prop change") || (
+                  <ReactFullpage.Wrapper>
+                    {
+                      <div>
+                        <div className="section" data-anchor="">
+                          <div className="content">
+                            <Icon
+                              inverted
+                              name="chevron down"
+                              className="go-down bounce"
+                            />
+                          </div>
+                          <div className="lowpoly"></div>
+                        </div>
+                        <div className="section" data-anchor="">
+                          <div className="content"></div>
+                          <div className="lowpoly"></div>
+                        </div>
+                        <div className="section" data-anchor="">
+                          <div className="content"></div>
+                          <div className="lowpoly"></div>
+                        </div>
+                        <div className="section" data-anchor="">
+                          <div className="content"></div>
+                          <div className="lowpoly"></div>
+                        </div>
+                      </div>
+                    }
+                  </ReactFullpage.Wrapper>
+                )
+              }
+            />
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
